e7d8b50826276883d5ee253da943434442bbfa99
Anatoliy <antononanatoliy82@gmail.com>
2022-12-17 00:58:32 +0500

    feat: Checking the correct of the path has been implement

--- src/command_processor/cd.js
+++ src/command_processor/cd.js
@@ -1,14 +1,23 @@
+import fs from 'fs';
 import process from 'process';
 import { lastDirectory } from '../start_fm/path_generator.js';
 import { start, last } from '../start_fm/start_path.js';
 
-export const cd = (path_jump) => {
-    const start_trace = start();
-    try {  
+export const cd = async (path_jump) => {
+    const start_trace = start();    
+    try {
         const path_next = lastDirectory(start_trace, path_jump);
-        last(path_next);
-        process.stdout.write(`You are currently in path: ${path_next}\n
+        fs.access(path_next, (error) => {
+            if (error) {
+                process.stdout.write('> PATH NOT CORRECT <\n');
+                process.stdout.write(`You are currently in path: ${start()}\n
 Enter command or "help" for a list of commands: `);
+                return;
+            }
+            last(path_next);
+            process.stdout.write(`You are currently in path: ${path_next}\n
+Enter command or "help" for a list of commands: `);         
+        });
     }
     catch {
         process.stdout.write(`Error...`);
