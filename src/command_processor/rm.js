e87b7fe4f3f62f0f4f6512bef6f1e8ad805d8897
Anatoliy <antononanatoliy82@gmail.com>
2022-12-18 00:39:02 +0500

    feat: add module RM - delete file

--- src/command_processor/rm.js
+++ src/command_processor/rm.js
@@ -1,12 +1,28 @@
+import fs, { constants } from 'fs';
+import { start } from '../start_fm/start_path.js';
 import { lastDirectory } from '../start_fm/path_generator.js';
 
-const args = process.argv.slice(2).toString();
-
-const rm =  () => {
-    const path_next = lastDirectory();
-    console.log('function cat: ' + args);
-    process.stdout.write(`You are currently in path: ${1}\n
+export const rm =  (path_file_for_delete) => {
+    const path_now = start();
+    try {
+        const path_next = lastDirectory(path_now, path_file_for_delete);
+        fs.access(path_next, constants.F_OK, (error) => {
+            if (error) {
+                console.log(`File ${path_file_for_delete} missing\n`);
+                process.stdout.write(`You are currently in path: ${path_now}\n
 Enter command or "help" for a list of commands: `);
-}
+            } else {
+                fs.unlink(path_next, (error) => {
+                    if (error) console.log ('FS operation failed');
+                });
+                process.stdout.write(`File ${path_file_for_delete} delete`);
+                process.stdout.write(`You are currently in path: ${path_now}\n
+Enter command or "help" for a list of commands: `);
+            }
+        });
 
-rm();
+    }
+    catch {
+        catch_err(path_now);
+    }
+}
